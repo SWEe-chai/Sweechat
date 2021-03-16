e2a47c9be3cc010bdc262311514c5560ae078a81
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-03-16 12:32:45 +0800

    feat: handle message sending

--- Sweechat/ViewModels/ChatRoomViewModel.swift
+++ Sweechat/ViewModels/ChatRoomViewModel.swift
@@ -5,7 +5,11 @@ class ChatRoomViewModel: ObservableObject {
     private let db = Firestore.firestore()
     private var reference: CollectionReference?
 
-    @Published var chatRoom: ChatRoom
+    @Published var chatRoom: ChatRoom {
+        didSet {
+            objectWillChange.send()
+        }
+    }
     private var messageListener: ListenerRegistration?
 
     var text: String {
@@ -32,27 +36,54 @@ class ChatRoomViewModel: ObservableObject {
 
     }
 
+    func handleSendMessage(_ text: String) {
+        // TODO: Dont hardcode
+        let user = User(id: "abc", firstName: "first", lastName: "last")
+        let message = Message(user: user, content: text)
+        print("sebelom")
+        print(chatRoom.messages)
+        save(message)
+        print("sesuda")
+        print(chatRoom.messages)
+    }
+
+    private func save(_ message: Message) {
+        reference?.addDocument(data: MessageAdapter.convert(message: message)) { error in
+            if let e = error {
+                print("Error sending message: \(e.localizedDescription)")
+                return
+            }
+        }
+    }
+
     private func insertNewMessage(_ message: Message) {
+        print("kenapa ga kesini")
       guard !chatRoom.messages.contains(message) else {
         return
       }
 
       chatRoom.messages.append(message)
+        print("kenapa ga kesini")
+        print(chatRoom.messages)
       chatRoom.messages.sort()
-    }
+
+        print("kenapa ga kesini3")
+}
 
     private func handleDocumentChange(_ change: DocumentChange) {
+        print("kesini gaaaa")
+        print(chatRoom.messages)
         guard let message = MessageAdapter.convert(document: change.document) else {
-        return
-      }
+            return
+        }
 
-      switch change.type {
-      case .added:
-          insertNewMessage(message)
+        switch change.type {
+        case .added:
+            insertNewMessage(message)
 
-      default:
-        break
-      }
+        default:
+            break
+        }
     }
 
 }
