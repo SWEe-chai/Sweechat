4bce631835404458552ece20050254f08250dbba
hai <1503.hai.nguyen@gmail.com>
2021-03-19 14:20:47 +0800

    refactor: use database constants for users

--- Sweechat/Models/User/FirebaseUserFacade.swift
+++ Sweechat/Models/User/FirebaseUserFacade.swift
@@ -11,7 +11,7 @@ class FirebaseUserFacade: UserFacade {
 
     func loginAsUser(withDetails details: UserDetails) {
         userId = details.id
-        db.collection("users").document(userId).getDocument { document, _ in
+        db.collection(DatabaseConstant.Collection.users).document(userId).getDocument { document, _ in
             guard let document = document,
                   document.exists else {
                 // In this case user is a new user
@@ -23,26 +23,26 @@ class FirebaseUserFacade: UserFacade {
     }
 
     private func addUser(withDetails details: UserDetails) {
-        db.collection("users").document(details.id).setData([
-                    "id": details.id,
-                    "name": details.name,
-                    "profilePictureURL": details.profilePictureURL
+        db.collection(DatabaseConstant.Collection.users).document(details.id).setData([
+                    DatabaseConstant.User.id: details.id,
+                    DatabaseConstant.User.name: details.name,
+                    DatabaseConstant.User.profilePictureURL: details.profilePictureURL
         ], completion: { _ in
             self.setUpConnectionAsUser()
         })
     }
 
     private func setUpConnectionAsUser() {
-        reference = db.collection("users").document(userId)
+        reference = db.collection(DatabaseConstant.Collection.users).document(userId)
         messageListener = reference?.addSnapshotListener { querySnapshot, error in
             guard let snapshot = querySnapshot,
                   let data = snapshot.data() else {
                 print("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
                 return
             }
-            guard let id = data["id"] as? String,
-                  let name = data["name"] as? String,
-                  let profilePictureURL = data["profilePictureURL"] as? String else {
+            guard let id = data[DatabaseConstant.User.id] as? String,
+                  let name = data[DatabaseConstant.User.name] as? String,
+                  let profilePictureURL = data[DatabaseConstant.User.profilePictureURL] as? String else {
                 print("Error reading data update for user")
                 return
             }
