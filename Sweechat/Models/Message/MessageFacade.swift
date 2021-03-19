07ba63e965457d961e9e5e8d902c7f08fc898c5e
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-03-19 23:28:01 +0800

    feat: refactor message libraries and add facade

---
+++ Sweechat/Models/Message/MessageFacade.swift
@@ -0,0 +1,9 @@
+//
+//  MessageFacade.swift
+//  Sweechat
+//
+//  Created by Agnes Natasya on 19/3/21.
+//
+protocol MessageFacade {
+    var delegate: MessageFacadeDelegate? { get set }
+}
---
+++ Sweechat/Models/Message/MessageFacadeDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  MessageFacadeDelegate.swift
+//  Sweechat
+//
+//  Created by Agnes Natasya on 19/3/21.
+//
+protocol MessageFacadeDelegate: AnyObject {
+}
