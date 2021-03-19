cfcbfc36c4e4230c4b1674491c4cc5c4a0162456
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-03-19 23:28:16 +0800

    feat: add dummy consts

---
+++ Sweechat/Constants/AppConstant.swift
@@ -0,0 +1,10 @@
+//
+//  AppConstant.swift
+//  Sweechat
+//
+//  Created by Agnes Natasya on 18/3/21.
+//
+// TODO: Connect with Auth
+struct AppConstant {
+    static let user = FirebaseUserFacade.getUserDetails(id: "jm9bz28b06YV923yN0ydixzcMIm2")!
+}
