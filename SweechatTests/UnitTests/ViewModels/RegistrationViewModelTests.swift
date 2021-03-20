cdeb35853b901ce82dc520917a162958351d114e
Kevin Lim <lyskevin@gmail.com>
2021-03-20 17:41:02 +0800

    test: add registration vm tests

---
+++ SweechatTests/UnitTests/ViewModels/RegistrationViewModelTests.swift
@@ -0,0 +1,33 @@
+import XCTest
+@testable import Sweechat
+
+class RegistrationViewModelTests: XCTestCase {
+    private class LoggedInDelegateStub: LoggedInDelegate {
+        var didNavigateToHome = false
+
+        func navigateToHome() {
+            didNavigateToHome = true
+        }
+    }
+
+    private var delegateStub: LoggedInDelegateStub!
+    private var sut: RegistrationViewModel!
+
+    override func setUp() {
+        super.setUp()
+        delegateStub = LoggedInDelegateStub()
+        sut = RegistrationViewModel()
+        sut.delegate = delegateStub
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testDidTapEntryButton_callsDelegateENtryMethod() {
+        sut.didTapHomeButton()
+
+        XCTAssertTrue(delegateStub.didNavigateToHome)
+    }
+}
