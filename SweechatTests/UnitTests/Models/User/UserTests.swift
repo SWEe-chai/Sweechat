c076f74b25ea0fb69f637df6f1be0d128fe6a48f
Lim Yong Shen, Kevin <lyskevin@gmail.com>
2021-03-20 14:46:43 +0800

    test: add unit tests for user model (#25)

---
+++ SweechatTests/UnitTests/Models/User/UserTests.swift
@@ -0,0 +1,60 @@
+import XCTest
+@testable import Sweechat
+
+class UserTests: XCTestCase {
+    private var details = UserRepresentation(id: "1", name: "user", profilePictureUrl: "url")
+    private var sut: User!
+
+    override func setUp() {
+        super.setUp()
+        sut = User(details: details)
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testCreateUser_returnsUserWithDummyUserDetails() {
+        let user = User.createDummyUser()
+
+        XCTAssertEqual(user.id, User.dummyUserId)
+        XCTAssertEqual(user.name, User.dummyUserName)
+        XCTAssertNil(user.profilePictureUrl)
+        XCTAssertFalse(user.isLoggedIn)
+    }
+
+    func testSubscribeToSignedIn_callsFunctionUponChangingLoggedInStatus() {
+        var isFunctionCalled = false
+        let function: (Bool) -> Void = { _ in
+            isFunctionCalled = true
+        }
+        _ = sut.subscribeToSignedIn(function: function)
+
+        sut.isLoggedIn = true
+
+        XCTAssertTrue(isFunctionCalled)
+    }
+
+    func testUpdateUserData_allDetailsFilled_updatesUserDataWithDetails() {
+        let newDetails = UserRepresentation(id: "2", name: "name", profilePictureUrl: "pic", isLoggedIn: true)
+
+        sut.updateUserData(withDetails: newDetails)
+
+        XCTAssertEqual(sut.id, newDetails.id)
+        XCTAssertEqual(sut.name, newDetails.name)
+        XCTAssertEqual(sut.profilePictureUrl, newDetails.profilePictureUrl)
+        XCTAssertEqual(sut.isLoggedIn, newDetails.isLoggedIn)
+    }
+
+    func testUpdateUserData_nilProfilePictureUrl_updatesUserDataWithDetailsAndNilProfilePictureUrl() {
+        let newDetails = UserRepresentation(id: "2", name: "name", profilePictureUrl: nil, isLoggedIn: true)
+
+        sut.updateUserData(withDetails: newDetails)
+
+        XCTAssertEqual(sut.id, newDetails.id)
+        XCTAssertEqual(sut.name, newDetails.name)
+        XCTAssertEqual(sut.profilePictureUrl, newDetails.profilePictureUrl)
+        XCTAssertEqual(sut.isLoggedIn, newDetails.isLoggedIn)
+    }
+}
