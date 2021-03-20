89170f91ab9399cd69a938a86e1ffdd3da35383f
Lim Yong Shen, Kevin <lyskevin@gmail.com>
2021-03-20 15:22:40 +0800

    test: add message unit tests (#26)

--- Sweechat/Models/Message/Message.swift
+++ Sweechat/Models/Message/Message.swift
@@ -1,10 +1,3 @@
-//
-//  Message.swift
-//  Sweechat
-//
-//  Created by Agnes Natasya on 14/3/21.
-//
-
 import Foundation
 
 struct Message {
@@ -15,13 +8,6 @@ struct Message {
     var type: MessageType
     var downloadURL: URL?
 
-//  var data: MessageData {
-//    if let image = image {
-//      return .photo(image)
-//    } else {
-//      return .text(content)
-//    }
-//  }
     init(sender: User, content: String) {
         self.sender = sender
         self.content = content
@@ -37,67 +23,9 @@ struct Message {
         self.content = content
         self.type = MessageType.text
     }
-//  init(user: User, image: UIImage) {
-//    sender = Sender(id: user.uid, displayName: AppSettings.displayName)
-//    self.image = image
-//    content = ""
-//    sentDate = Date()
-//    id = nil
-//  }
-
-//  init?(document: QueryDocumentSnapshot) {
-//    let data = document.data()
-//
-//    guard let sentDate = data["created"] as? Date else {
-//      return nil
-//    }
-//    guard let senderID = data["senderID"] as? String else {
-//      return nil
-//    }
-//    guard let senderName = data["senderName"] as? String else {
-//      return nil
-//    }
-//
-//    id = document.documentID
-//
-//    self.sentDate = sentDate
-//    sender = Sender(id: senderID, displayName: senderName)
-//
-//    if let content = data["content"] as? String {
-//      self.content = content
-//      downloadURL = nil
-//    } else if let urlString = data["url"] as? String, let url = URL(string: urlString) {
-//      downloadURL = url
-//      content = ""
-//    } else {
-//      return nil
-//    }
-//  }
-
 }
 
-// extension Message: DatabaseRepresentation {
-//
-//  var representation: [String : Any] {
-//    var rep: [String : Any] = [
-//      "created": sentDate,
-//      "senderID": sender.id,
-//      "senderName": sender.displayName
-//    ]
-//
-//    if let url = downloadURL {
-//      rep["url"] = url.absoluteString
-//    } else {
-//      rep["content"] = content
-//    }
-//
-//    return rep
-//  }
-//
-// }
-
 extension Message: Comparable {
-
     static func == (lhs: Message, rhs: Message) -> Bool {
         lhs.id == rhs.id
     }
@@ -105,5 +33,4 @@ extension Message: Comparable {
     static func < (lhs: Message, rhs: Message) -> Bool {
         lhs.creationTime < rhs.creationTime
     }
-
 }
