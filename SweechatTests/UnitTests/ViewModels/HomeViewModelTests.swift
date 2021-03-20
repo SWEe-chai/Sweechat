1b4ebbb48db5180c470fb64e3eaec0df01a7ac13
Kevin Lim <lyskevin@gmail.com>
2021-03-20 21:49:20 +0800

    test: add home vm tests

---
+++ SweechatTests/UnitTests/ViewModels/HomeViewModelTests.swift
@@ -0,0 +1,51 @@
+import XCTest
+@testable import Sweechat
+
+class HomeViewModelTests: XCTestCase {
+    private class UserStub: User {
+        init() {
+            super.init(details: UserRepresentation(id: User.dummyUserId, name: User.dummyUserName,
+                                                   profilePictureUrl: nil, isLoggedIn: false))
+        }
+    }
+
+    private class HomeDelegateStub: HomeDelegate {
+        var didNavigateToSettings = false
+        var didNavigateToModule = false
+
+        func navigateToSettingsFromHome() {
+            didNavigateToSettings = true
+        }
+
+        func navigateToModuleFromHome() {
+            didNavigateToModule = true
+        }
+    }
+
+    private var delegateStub: HomeDelegateStub!
+    private var sut: HomeViewModel!
+
+    override func setUp() {
+        super.setUp()
+        delegateStub = HomeDelegateStub()
+        sut = HomeViewModel(user: UserStub())
+        sut.delegate = delegateStub
+    }
+
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+
+    func testDidTapModuleButton_callsDelegateModuleMethod() {
+        sut.didTapModuleButton()
+
+        XCTAssertTrue(delegateStub.didNavigateToModule)
+    }
+
+    func testDidTapSettingsButton_callsDelegateSettingsMethod() {
+        sut.didTapSettingsButton()
+
+        XCTAssertTrue(delegateStub.didNavigateToSettings)
+    }
+}
