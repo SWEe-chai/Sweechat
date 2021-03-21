a659cfe75ea114beb76f765d9e785dab36d84a43
Kevin Lim <lyskevin@gmail.com>
2021-03-21 18:18:02 +0800

    test: add app vm unit tests for state changing from invalid states

--- SweechatTests/UnitTests/ViewModels/AppViewModelTests.swift
+++ SweechatTests/UnitTests/ViewModels/AppViewModelTests.swift
@@ -17,7 +17,7 @@ class AppViewModelTests: XCTestCase {
         super.tearDown()
     }
 
-    func testNavigateToEntry() {
+    func testNavigateToEntry_validState_changesToEntryState() {
         sut.state = AppState.home // set some other initial state in AppVM
 
         sut.navigateToEntryFromLoggedOut()
@@ -25,43 +25,100 @@ class AppViewModelTests: XCTestCase {
         XCTAssertEqual(sut.state, AppState.entry)
     }
 
-    func testNavigateToLogin() {
+    func testNavigateToEntry_invalidState_changesToDefaultLoggedInState() {
+        sut.signIn(withDetails: alLogInDetails)
+        sut.state = AppState.module // set some other initial state in AppVM
+
+        sut.navigateToEntryFromLoggedOut()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedInAppState)
+    }
+
+    func testNavigateToLogin_validState_changesToLoginState() {
         sut.navigateToLoginFromEntry()
 
         XCTAssertEqual(sut.state, AppState.login)
     }
 
-    func testNavigateToRegistration() {
+    func testNavigateToLogin_invalidState_changesToDefaultLoggedInState() {
+        sut.signIn(withDetails: alLogInDetails)
+
+        sut.navigateToLoginFromEntry()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedInAppState)
+    }
+
+    func testNavigateToRegistration_validState_changesToRegistrationState() {
         sut.navigateToRegistrationFromEntry()
 
         XCTAssertEqual(sut.state, AppState.registration)
     }
 
-    func testNavigateToChatRoom() {
+    func testNavigateToRegistration_invalidState_changesToDefaultLoggedInState() {
+        sut.signIn(withDetails: alLogInDetails)
+
+        sut.navigateToRegistrationFromEntry()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedInAppState)
+    }
+
+    func testNavigateToChatRoom_validState_changesToChatRoomState() {
         sut.signIn(withDetails: alLogInDetails)
         sut.navigateToChatRoomFromModule()
 
         XCTAssertEqual(sut.state, AppState.chatRoom)
     }
 
-    func testNavigateToHome() {
+    func testNavigateToChatRoom_invalidState_changesToDefaultLoggedOutState() {
+        sut.state = AppState.login // set some other initial state in AppVM
+
+        sut.navigateToChatRoomFromModule()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedOutAppState)
+    }
+
+    func testNavigateToHome_validState_changesToHomeState() {
         sut.signIn(withDetails: alLogInDetails)
         sut.navigateToHomeFromLoggedIn()
 
         XCTAssertEqual(sut.state, AppState.home)
     }
 
-    func testNavigateToModule() {
+    func testNavigateToHome_invalidState_changesToDefaultLoggedOutState() {
+        sut.state = AppState.login // set some other initial state in AppVM
+
+        sut.navigateToHomeFromLoggedIn()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedOutAppState)
+    }
+
+    func testNavigateToModule_validState_changesToModuleState() {
         sut.signIn(withDetails: alLogInDetails)
         sut.navigateToModuleFromHome()
 
         XCTAssertEqual(sut.state, AppState.module)
     }
 
-    func testNavigateToSettings() {
+    func testNavigateToModule_invalidState_changesToDefaultLoggedOutState() {
+        sut.state = AppState.login // set some other initial state in AppVM
+
+        sut.navigateToModuleFromHome()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedOutAppState)
+    }
+
+    func testNavigateToSettings_validState_changesToSettingsState() {
         sut.signIn(withDetails: alLogInDetails)
         sut.navigateToSettingsFromHome()
 
         XCTAssertEqual(sut.state, AppState.settings)
+    }
+
+    func testNavigateToSettings_invalidState_changesToDefaultLoggedOutState() {
+        sut.state = AppState.login // set some other initial state in AppVM
+
+        sut.navigateToSettingsFromHome()
+
+        XCTAssertEqual(sut.state, StateConstant.DefaultLoggedOutAppState)
     }
 }
