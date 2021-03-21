b65eed29d3c55fee4fee2672d42f49e80ec49f8f
Christian James Welly <39089453+chrisjwelly@users.noreply.github.com>
2021-03-21 11:13:05 +0800

    fix: avoid changing state when isLoggedIn is still being set (#37)
    * fix: avoid changing state when isLoggedIn is still being set
    
    * chore: group variables in StateConstant

--- Sweechat/Constants/StateConstant.swift
+++ Sweechat/Constants/StateConstant.swift
@@ -7,14 +7,14 @@
 
 struct StateConstant {
     static let LoggedOutAppStates: Set<AppState> = [.onboarding, .entry, .login, .registration]
-    static let LoggedInAppStates: Set<AppState> = [.home, .module, .settings, .chatRoom]
     static let DefaultLoggedOutAppState: AppState = .entry
-    static let DefaultLoggedInAppState: AppState = .home
-
-    static let DefaultLoggedInAppStateMessage: StaticString = """
-        User is already logged in. Switching to default LoggedIn AppState.
-        """
     static let DefaultLoggedOutAppStateMessage: StaticString = """
         User is not yet logged in. Switching to default LoggedOut AppState.
+        """
+
+    static let LoggedInAppStates: Set<AppState> = [.home, .module, .settings, .chatRoom]
+    static let DefaultLoggedInAppState: AppState = .home
+    static let DefaultLoggedInAppStateMessage: StaticString = """
+        User is already logged in. Switching to default LoggedIn AppState.
         """
 }
