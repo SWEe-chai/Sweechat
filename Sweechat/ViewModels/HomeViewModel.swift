5475eeff6c0b8eb6b5d794e1aa7fa72b76401ff4
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-03-29 14:52:20 +0800

    feat: implement sign in with previous session and sign out (#62)
    * feat: auto login and signout
    
    * refactor: fix according to  comments
    
    * refactor: change name of function

--- Sweechat/ViewModels/HomeViewModel.swift
+++ Sweechat/ViewModels/HomeViewModel.swift
@@ -2,6 +2,7 @@ import Combine
 import Foundation
 class HomeViewModel: ObservableObject {
     var user: User
+    weak var delegate: HomeViewModelDelegate?
     var settingsViewModel: SettingsViewModel
     var moduleList: ModuleList
     @Published var text: String = ""
@@ -12,9 +13,8 @@ class HomeViewModel: ObservableObject {
         self.user = user
         self.text = "Welcome home \(user.name)"
         self.moduleList = ModuleList.of(user.id)
-        // TODO: Connect this Settings View Model if we want to
-        // implement logout
         self.settingsViewModel = SettingsViewModel()
+        settingsViewModel.delegate = self
         initialiseSubscribers()
     }
 
@@ -47,5 +47,12 @@ class HomeViewModel: ObservableObject {
     func handleJoinModule(secret: String) {
         let id = secret
         moduleList.joinModule(moduleId: id)
+    }
+}
+
+// MARK: SettingsViewModelDelegate
+extension HomeViewModel: SettingsViewModelDelegate {
+    func signOut() {
+        delegate?.signOut()
     }
 }
