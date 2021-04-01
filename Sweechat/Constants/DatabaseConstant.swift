2e330ae90288b26e46a77f2e06c7e6541f19e49c
Christian James Welly <chrisjwelly@gmail.com>
2021-04-01 19:15:44 +0800

    chore: change database environment

--- Sweechat/Constants/DatabaseConstant.swift
+++ Sweechat/Constants/DatabaseConstant.swift
@@ -10,7 +10,7 @@ import Foundation
 struct DatabaseConstant {
     struct Collection {
         static let environmentCollection = "environment"
-        static let dev = "hai-chatroom-permission"
+        static let dev = "christian-reply-message"
         // Change environmentDocument as needed when working on features
         // involving a schema change
         static let environmentDocument = dev
