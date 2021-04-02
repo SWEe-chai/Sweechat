ea27d38863a56563f90fde59f2018a026fb579c4
agnesnatasya <agnesnatasyawijaya@gmail.com>
2021-04-02 11:36:18 +0800

    feat: remove unneccessary comments

--- Sweechat/Views/ChatRoomViewFactory.swift
+++ Sweechat/Views/ChatRoomViewFactory.swift
@@ -4,7 +4,6 @@ struct ChatRoomViewFactory {
     static func makeView(viewModel: ChatRoomViewModel) -> some View {
         switch viewModel.permissions {
         case .normal:
-            // return DrawingView()
             return ChatRoomView(viewModel: viewModel)
         case .readOnly:
             // Return some AnnouncementsView here
