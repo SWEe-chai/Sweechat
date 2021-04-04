6a4bfdcb6f05ade0cb9612e063382a0be8b404ad
Christian James Welly <chrisjwelly@gmail.com>
2021-04-04 10:19:00 +0800

    feat: copy to clipboard upon long pressing module secret

--- Sweechat/Views/Module/ModuleInformation.swift
+++ Sweechat/Views/Module/ModuleInformation.swift
@@ -2,13 +2,25 @@ import SwiftUI
 
 struct ModuleInformation: View {
     @ObservedObject var viewModel: ModuleViewModel
+    @State var copied: Bool = false
+
     var body: some View {
         VStack {
             Text(viewModel.text).font(.title)
             Divider().padding()
             Text("Module secret:").font(.callout)
             Text("Other users can join this module by entering this Module secret").font(.caption)
-            Text("\(viewModel.id)").font(.title3).padding()
+            Text("Press and hold the secret to copy to clipboard:").font(.caption)
+            Text("\(viewModel.id)")
+                .font(.title3)
+                .padding()
+                .onLongPressGesture(minimumDuration: 0.5) {
+                    UIPasteboard.general.string = viewModel.id
+                    copied = true
+                }
+            if copied {
+                Text("Module Secret has been copied to clipboard!").font(.caption)
+            }
         }.navigationTitle("Module Information")
     }
 }
