61d2337ae6bcfb1b5781d4bd1e3a68f4d80006e7
Christian James Welly <chrisjwelly@gmail.com>
2021-04-06 23:24:54 +0800

    feat: add module permission

---
+++ Sweechat/Models/Module/ModulePermission.swift
@@ -0,0 +1,22 @@
+//
+//  ModulePermission.swift
+//  Sweechat
+//
+//  Created by Christian James Welly on 6/4/21.
+//
+
+import Foundation
+
+typealias ModulePermissionBitmask = UInt32
+
+struct ModulePermission {
+    static let none: ModulePermissionBitmask = 0
+    static let all: ModulePermissionBitmask = UInt32.max
+    static let privateChatRoomCreation: ModulePermissionBitmask = 0b1
+    static let groupChatRoomCreation: ModulePermissionBitmask = 0b1 << 1
+    static let announcementCreation: ModulePermissionBitmask = 0b1 << 2
+    static let forumCreation: ModulePermissionBitmask = 0b1 << 3
+
+    static let moduleOwner = all
+    static let student = privateChatRoomCreation | groupChatRoomCreation | forumCreation
+}
