736428da14dd4232a2b56970ff293a2d8f41ae15
Christian James Welly <chrisjwelly@gmail.com>
2021-04-11 17:12:23 +0800

    chore: change database constant

--- Sweechat/Constants/DatabaseConstant.swift
+++ Sweechat/Constants/DatabaseConstant.swift
@@ -10,7 +10,7 @@ import Foundation
 struct DatabaseConstant {
     struct Collection {
         static let environmentCollection = "environment"
-        static let dev = "forum-thread2"
+        static let dev = "type-safe-identifier"
         // Change environmentDocument as needed when working on features
         // involving a schema change
         static let environmentDocument = dev
