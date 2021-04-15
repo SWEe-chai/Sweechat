3895653b0d3eac21021dbc0cfc316e4ff110fd29
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-04-15 13:46:57 +0800

    feat: pagination (#113)
    * feat: added pagination backend
    
    * feat: scroll working
    
    * feat: everything works now
    
    * refactor: slim down view
    
    * feat: add button to load older messages
    
    * refactor: remove parent message from message
    
    * refactor: remove parent message from message
    
    * chore: remove unused firstHalf function
    
    * refactor: delete unused file
    
    * feat: add pagination for forum
    
    * refactor: check if adding 0 messages to see if all messages have been loaded
    
    * fix: firebase bug and parentId bug
    
    * fix: change date to timestamp
    
    * feat: add time to live for loading all for a certain message
    
    * fix: timestamp not timeinterval
    
    * refactor: refactor based on comments
    
    * refactor: change block size back to 20

--- Sweechat/Models/ChatRoom/FirebaseChatRoomFacade.swift
+++ Sweechat/Models/ChatRoom/FirebaseChatRoomFacade.swift
@@ -21,13 +21,17 @@ class FirebaseChatRoomFacade: ChatRoomFacade {
     private var chatRoomListener: ListenerRegistration?
     private var messagesReference: CollectionReference?
     private var filteredMessagesReference: Query?
-    private var messagesListener: ListenerRegistration?
+    private var messagesUpdateListener: ListenerRegistration?
+    private var messagesInsertListener: ListenerRegistration?
     private var userChatRoomModulePairsFilteredQuery: Query?
     private var userChatRoomModulePairsListener: ListenerRegistration?
+    private var oldestMessageDocument: QueryDocumentSnapshot?
+    private var blockSize: Int = 20
 
-    init(chatRoomId: Identifier<ChatRoom>, user: User) {
+    init(chatRoomId: Identifier<ChatRoom>, user: User, delegate: ChatRoomFacadeDelegate) {
         self.chatRoomId = chatRoomId
         self.user = user
+        self.delegate = delegate
         setUpConnectionToChatRoom()
     }
 
@@ -62,20 +66,14 @@ class FirebaseChatRoomFacade: ChatRoomFacade {
     }
 
     private func loadMembers(onCompletion: (() -> Void)?) {
-        userChatRoomModulePairsFilteredQuery?.getDocuments { querySnapshot, error in
-            guard let snapshot = querySnapshot else {
-                os_log("Error loading user module pairs: \(error?.localizedDescription ?? "No error")")
-                return
+        FirebaseUserChatRoomModulePairQuery
+            .getUserChatRoomModulePairs(inChatRoomId: chatRoomId) { pairs in
+                let userIds: [Identifier<User>] = pairs.map { $0.userId }
+                FirebaseUserQuery.getUsers(withIds: userIds) { users in
+                    self.delegate?.insertAll(members: users)
+                    onCompletion?()
+                }
             }
-            let userIds: [Identifier<User>] = snapshot.documents.compactMap {
-                $0.data()[DatabaseConstant.UserModulePair.userId] as? String
-            }
-            .map({ Identifier<User>(val: $0) })
-            FirebaseUserQuery.getUsers(withIds: userIds) { users in
-                self.delegate?.insertAll(members: users)
-                onCompletion?()
-            }
-        }
     }
 
     private func loadKeyExchangeMessages(onCompletion: (() -> Void)?) {
@@ -100,17 +98,22 @@ class FirebaseChatRoomFacade: ChatRoomFacade {
     }
 
     private func loadMessages(onCompletion: (() -> Void)?) {
-        filteredMessagesReference?.getDocuments { querySnapshot, error in
-            guard let snapshot = querySnapshot else {
-                os_log("Error loading messages: \(error?.localizedDescription ?? "No error")")
-                return
+        filteredMessagesReference?
+            .order(by: DatabaseConstant.Message.creationTime)
+            .limit(toLast: blockSize)
+            .getDocuments { querySnapshot, error in
+                guard let snapshot = querySnapshot else {
+                    os_log("Error loading messages: \(error?.localizedDescription ?? "No error")")
+                    onCompletion?()
+                    return
+                }
+                self.oldestMessageDocument = snapshot.documents.first
+                let messages = snapshot.documents.compactMap({
+                    FirebaseMessageFacade.convert(document: $0)
+                })
+                self.delegate?.insertAll(messages: messages)
+                onCompletion?()
             }
-            let messages = snapshot.documents.compactMap({
-                FirebaseMessageFacade.convert(document: $0)
-            })
-            self.delegate?.insertAll(messages: messages)
-            onCompletion?()
-        }
     }
 
     private func addListeners() {
@@ -120,15 +123,29 @@ class FirebaseChatRoomFacade: ChatRoomFacade {
             }
         }
 
-        messagesListener = filteredMessagesReference?.addSnapshotListener { querySnapshot, error in
-            guard let snapshot = querySnapshot else {
-                os_log("Error listening for channel updates: \(error?.localizedDescription ?? "No error")")
-                return
+        messagesUpdateListener = filteredMessagesReference?
+            .addSnapshotListener { querySnapshot, error in
+                guard let snapshot = querySnapshot else {
+                    os_log("Error listening for all messages: \(error?.localizedDescription ?? "No error")")
+                    return
+                }
+                snapshot.documentChanges
+                    .filter { $0.type != .added }
+                    .forEach { self.handleMessageDocumentChange($0) }
             }
-            snapshot.documentChanges.forEach { change in
-                self.handleMessageDocumentChange(change)
+
+        messagesInsertListener = filteredMessagesReference?
+            .order(by: DatabaseConstant.Message.creationTime)
+            .limit(toLast: 1)
+            .addSnapshotListener { querySnapshot, error in
+                guard let snapshot = querySnapshot else {
+                    os_log("Error listening for new messages: \(error?.localizedDescription ?? "No error")")
+                    return
+                }
+                snapshot.documentChanges
+                    .filter { $0.type == .added }
+                    .forEach { self.handleMessageDocumentChange($0) }
             }
-        }
 
         userChatRoomModulePairsListener = userChatRoomModulePairsFilteredQuery?
             .addSnapshotListener { querySnapshot, error in
@@ -140,6 +157,73 @@ class FirebaseChatRoomFacade: ChatRoomFacade {
                     self.handleUserModulePairDocumentChange(change)
                 }
             }
+    }
+
+    func loadNextBlock(onCompletion: @escaping ([Message]) -> Void) {
+        guard let oldestMessageDocument = self.oldestMessageDocument else {
+            os_log("Trying to load next block but not available")
+            onCompletion([])
+            return
+        }
+        filteredMessagesReference?
+            .order(by: DatabaseConstant.Message.creationTime)
+            .end(beforeDocument: oldestMessageDocument)
+            .limit(toLast: blockSize)
+            .getDocuments { querySnapshot, error in
+                guard let snapshot = querySnapshot,
+                      let oldestMessageDocument = snapshot.documents.first else {
+                    os_log("No more messages: \(error?.localizedDescription ?? "No error")")
+                    onCompletion([])
+                    return
+                }
+                self.compareAndSetOldestMessageDocument(oldestMessageDocument)
+                let messages = snapshot.documents.compactMap({
+                    FirebaseMessageFacade.convert(document: $0)
+                })
+                onCompletion(messages)
+            }
+    }
+
+    func loadMessage(withId id: String, onCompletion: @escaping (Message?) -> Void) {
+        messagesReference?
+            .document(id)
+            .getDocument { querySnapshot, error in
+                guard let snapshot = querySnapshot else {
+                    os_log("Error loading messages: \(error?.localizedDescription ?? "No error")")
+                    onCompletion(nil)
+                    return
+                }
+                onCompletion(FirebaseMessageFacade.convert(document: snapshot))
+            }
+    }
+
+    func loadUntil(_ time: Date, onCompletion: @escaping ([Message]) -> Void) {
+        let timestamp = Timestamp(date: time)
+        filteredMessagesReference?
+            .order(by: DatabaseConstant.Message.creationTime)
+            .start(at: [timestamp])
+            .getDocuments { querySnapshot, error in
+                guard let snapshot = querySnapshot,
+                      let oldestMessageDocument = snapshot.documents.first else {
+                    os_log("Error loading messages: \(error?.localizedDescription ?? "No error")")
+                    onCompletion([])
+                    return
+                }
+                self.compareAndSetOldestMessageDocument(oldestMessageDocument)
+                let messages = snapshot.documents.compactMap({
+                    FirebaseMessageFacade.convert(document: $0)
+                })
+                onCompletion(messages)
+            }
+    }
+
+    private func compareAndSetOldestMessageDocument(_ oldestMessageDocument: QueryDocumentSnapshot) {
+        guard let previousTime = self.oldestMessageDocument?[DatabaseConstant.Message.creationTime] as? Timestamp,
+              let newTime = oldestMessageDocument[DatabaseConstant.Message.creationTime] as? Timestamp,
+              newTime.dateValue() < previousTime.dateValue() else {
+            return
+        }
+        self.oldestMessageDocument = oldestMessageDocument
     }
 
     func save(_ message: Message) {
--- Sweechat/Views/ChatRoom/MessagesScrollView.swift
+++ Sweechat/Views/ChatRoom/MessagesScrollView.swift
@@ -5,52 +5,61 @@ struct MessagesScrollView: View {
     @ObservedObject var viewModel: ChatRoomViewModel
     @Binding var parentPreviewMetadata: ParentPreviewMetadata?
 
+    @State private var scrollOffset: CGFloat = .zero
+    @State private var heightOffset: CGFloat = .zero
+    @State private var canLoadMore: Bool = true
+
     var body: some View {
-        ScrollView {
+        ScrollViewOffset(offset: $scrollOffset, height: $heightOffset) {
             ScrollViewReader { scrollView in
-                ForEach(viewModel.messages, id: \.self) { messageViewModel in
-                    let parentMessage = getMessage(withId: messageViewModel.parentId)
-                    MessageView(viewModel: messageViewModel,
-                                parentViewModel: parentMessage, parentPreviewMetadata: $parentPreviewMetadata,
-                                onReplyPreviewTapped: { scrollToMessage(scrollView, parentMessage) })
+                LazyVStack {
+                    if !viewModel.areAllMessagesLoaded {
+                        Button(action: viewModel.loadMore) { Text("older messages...") }.padding()
+                    }
+                    ForEach(viewModel.messages, id: \.self) { messageViewModel in
+                        let parentMessage = viewModel.getMessageViewModel(withId: messageViewModel.parentId)
+                        MessageView(viewModel: messageViewModel,
+                                    parentViewModel: parentMessage, parentPreviewMetadata: $parentPreviewMetadata,
+                                    onReplyPreviewTapped: { scrollToMessage(scrollView, parentMessage, anchor: .bottom) })
+                    }
                 }
-                .onAppear { scrollToLatestMessage(scrollView) }
-                .onChange(of: viewModel.messages.count) { _ in
-                    scrollToLatestMessage(scrollView)
+                .onAppear { scrollToMessage(scrollView, viewModel.messages.last, anchor: .bottom) }
+                .onChange(of: viewModel.latestMessageViewModel) { _ in
+                    handleNewMessages(scrollView)
                 }
                 .onChange(of: parentPreviewMetadata?.tappedPreview) { _ in
-                    guard let metadata = parentPreviewMetadata else {
-                        os_log("Info: parentPreviewMetadata is nil when detecting change.")
-                        return
-                    }
-
-                    if metadata.tappedPreview {
-                        scrollToMessage(scrollView, metadata.parentMessage)
-                        parentPreviewMetadata?.tappedPreview = false // value-type semantics. change directly
-                    }
+                    handleTappedPreview(scrollView)
                 }
                 .padding([.leading, .trailing])
             }
         }
     }
 
-    func scrollToLatestMessage(_ scrollView: ScrollViewProxy) {
-        if viewModel.messages.isEmpty {
+    func handleTappedPreview(_ scrollView: ScrollViewProxy) {
+        guard let metadata = parentPreviewMetadata else {
+            os_log("Info: parentPreviewMetadata is nil when detecting change.")
             return
         }
-        let index = viewModel.messages.count - 1
-        scrollView.scrollTo(viewModel.messages[index])
 
+        if metadata.tappedPreview {
+            scrollToMessage(scrollView, metadata.parentMessage, anchor: .bottom)
+            parentPreviewMetadata?.tappedPreview = false // value-type semantics. change directly
+        }
     }
 
-    private func getMessage(withId id: String?) -> MessageViewModel? {
-        viewModel.messages.first {
-            $0.id == id
+    func handleNewMessages(_ scrollView: ScrollViewProxy) {
+        if scrollOffset > heightOffset - UIScreen.main.bounds.height - 300 {
+             // in vicinity of the bottom and we get a new message
+            scrollToMessage(scrollView, viewModel.messages.last, anchor: .bottom)
         }
     }
 
     // TODO: Perhaps combine this with `scrollToLatesMessage`?
-    private func scrollToMessage(_ scrollView: ScrollViewProxy, _ message: MessageViewModel?) {
+    private func scrollToMessage(_ scrollView: ScrollViewProxy,
+                                 _ message: MessageViewModel?,
+                                 anchor: UnitPoint) {
+        os_log("Scrolling to \(message?.id ?? "nil message")")
+
         if viewModel.messages.isEmpty {
             os_log("messages are empty")
             return
@@ -59,12 +68,36 @@ struct MessagesScrollView: View {
             os_log("nil MessageViewModel passed into scrollToMessage")
             return
         }
-        guard let index = viewModel.messages.firstIndex(of: message) else {
-            os_log("could not find message in the list of messages")
+
+        if viewModel.messages.contains(message) {
+            scrollView.scrollTo(message, anchor: anchor)
             return
         }
-        withAnimation(Animation.easeIn(duration: 1.0)) {
-            scrollView.scrollTo(viewModel.messages[index], anchor: .bottom)
+
+        viewModel.loadUntil(messageViewModel: message)
+
+        checkAsync(interval: 0.1) {
+            if viewModel.messages.contains(message) {
+                withAnimation(Animation.easeIn(duration: 0.5)) {
+                    scrollView.scrollTo(message, anchor: anchor)
+                }
+                return false
+            }
+            return true
+        }
+    }
+
+    func checkAsync(interval: Double,
+                    repeatableFunction: @escaping () -> Bool,
+                    timeToLive: Int = 15) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            if timeToLive <= 0 {
+                os_log("Check async function ran total time to live and terminated")
+                return
+            }
+            if repeatableFunction() {
+                self.checkAsync(interval: interval, repeatableFunction: repeatableFunction, timeToLive: timeToLive - 1)
+            }
         }
     }
 }
@@ -84,18 +117,3 @@ struct MessagesScrollView_Previews: PreviewProvider {
         )
     }
 }
-
-// TODO: Currently removed because it is complaining due to the messageBeingRepliedTo
-// struct MessagesScrollView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        MessagesScrollView(
-//            viewModel: ChatRoomViewModel(
-//                chatRoom: ChatRoom(id: "0",
-//                                   name: "CS4269",
-//                                   currentUser: User(id: "", name: "Hello", profilePictureUrl: ""),
-//                                   currentUserPermission: ChatRoomPermission.readWrite),
-//                user: User(id: "", name: "Hello", profilePictureUrl: "")
-//            )
-//        )
-//    }
-// }
