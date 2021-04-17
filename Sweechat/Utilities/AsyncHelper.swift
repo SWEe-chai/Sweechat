ef934af8111c2e556d5a9285a4c9ba6838f3d16e
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-04-17 23:25:00 +0800

    fix: notification bug (#132)
    * fix: fix notif bug open from other places
    
    * refactor: remove prints
    
    * chore: remove unused boi
    
    * chore: make changes simpler
    
    * chore: remove extra debug buttons
    
    * refactor: remove redundant geometry reader
    
    * refactor: final touches
    
    * refactor: remove notification metadata from modulevm and add time to live

--- Sweechat/Utilities/AsyncHelper.swift
+++ Sweechat/Utilities/AsyncHelper.swift
@@ -10,10 +10,18 @@ import Foundation
 struct AsyncHelper {
     static let shortInterval = 0.01
     static let mediumInterval = 0.1
-    static func checkAsync(interval: Double, repeatableFunction: @escaping () -> Bool) {
+    static let longInterval = 1.0
+    static func checkAsync(interval: Double,
+                           repeatableFunction: @escaping () -> Bool,
+                           timeToLive: Int = 50) {
+        if timeToLive == 0 {
+            return
+        }
         DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
             if repeatableFunction() {
-                AsyncHelper.checkAsync(interval: interval, repeatableFunction: repeatableFunction)
+                AsyncHelper.checkAsync(interval: interval,
+                                       repeatableFunction: repeatableFunction,
+                                       timeToLive: timeToLive - 1)
             }
         }
     }
