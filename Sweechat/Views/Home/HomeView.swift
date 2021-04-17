6f14c846ad82bffa0a26580898b4d0704ffe08eb
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-04-17 22:37:16 +0800

    refactor: remove redundant geometry reader

--- Sweechat/Views/Home/HomeView.swift
+++ Sweechat/Views/Home/HomeView.swift
@@ -16,44 +16,41 @@ struct HomeView: View {
     }
 
     var body: some View {
-        GeometryReader { geometry in
-            VStack {
-                if isShowingCreateView {
-                    CreateModuleView(viewModel: viewModel)
-                } else {
-                    JoinModuleView(viewModel: viewModel)
-                }
-                VStack(alignment: .leading, spacing: 0) {
-                    if !viewModel.moduleViewModels.isEmpty {
-                        Text("Modules")
-                            .font(FontConstant.Heading1)
-                            .foregroundColor(ColorConstant.dark)
-                            .padding(.horizontal)
-                    }
-                    ScrollView(showsIndicators: false) {
-                        ForEach(
-                            Array(
-                                viewModel.moduleViewModels.enumerated()), id: \.offset) { index, moduleViewModel in
-                            Button(action: {
-                                viewModel.directModuleViewModel = moduleViewModel
-                                viewModel.isDirectModuleLoaded = true
-                            }) {
-                                ModuleItemView(viewModel: moduleViewModel, index: index)
-                            }
-                        }
-                        .padding()
-                        NavigationLink(
-                            "",
-                            destination: LazyNavView(
-                                ModuleView(viewModel: viewModel.directModuleViewModel)),
-                            isActive: $viewModel.isDirectModuleLoaded
-                        )
-                    }
-                    .padding(.top, 3)
-                }
-                Spacer()
+        VStack {
+            if isShowingCreateView {
+                CreateModuleView(viewModel: viewModel)
+            } else {
+                JoinModuleView(viewModel: viewModel)
             }
-            .frame(width: geometry.size.width)
+            VStack(alignment: .leading, spacing: 0) {
+                if !viewModel.moduleViewModels.isEmpty {
+                    Text("Modules")
+                        .font(FontConstant.Heading1)
+                        .foregroundColor(ColorConstant.dark)
+                        .padding(.horizontal)
+                }
+                ScrollView(showsIndicators: false) {
+                    ForEach(
+                        Array(
+                            viewModel.moduleViewModels.enumerated()), id: \.offset) { index, moduleViewModel in
+                        Button(action: {
+                            viewModel.directModuleViewModel = moduleViewModel
+                            viewModel.isDirectModuleLoaded = true
+                        }) {
+                            ModuleItemView(viewModel: moduleViewModel, index: index)
+                        }
+                    }
+                    .padding()
+                    NavigationLink(
+                        "",
+                        destination: LazyNavView(
+                            ModuleView(viewModel: viewModel.directModuleViewModel)),
+                        isActive: $viewModel.isDirectModuleLoaded
+                    )
+                }
+                .padding(.top, 3)
+            }
+            Spacer()
         }
         .background(ColorConstant.base)
         .toolbar {
