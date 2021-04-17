c0623b2c7e948a3fd95b6ad85da827ec63a6dbf2
Kevin Lim <lyskevin@gmail.com>
2021-04-17 13:49:04 +0800

    refactor: refactor FirebaseChatRoomQuery

--- Sweechat/Models/ChatRoom/FirebaseChatRoomQuery.swift
+++ Sweechat/Models/ChatRoom/FirebaseChatRoomQuery.swift
@@ -6,56 +6,60 @@ class FirebaseChatRoomQuery {
     static func getChatRoom(chatRoomId: Identifier<ChatRoom>,
                             user: User,
                             onCompletion: @escaping (ChatRoom) -> Void) {
-        FirebaseUserChatRoomModulePairQuery
-            .getUserChatRoomModulePair(chatRoomId: chatRoomId, userId: user.id) { pair in
-                guard let pair = pair else {
-                    return
-                }
+        FirebaseUserChatRoomModulePairQuery.getUserChatRoomModulePair(chatRoomId: chatRoomId, userId: user.id) { pair in
+            if let pair = pair {
                 getChatRoom(pair: pair, user: user) { chatRoom in
                     onCompletion(chatRoom)
                 }
             }
+        }
     }
+
     static func getChatRoom(pair: FirebaseUserChatRoomModulePair,
                             user: User,
                             onCompletion: @escaping (ChatRoom) -> Void) {
         getChatRooms(pairs: [pair], user: user) { chatRooms in
-            guard let chatRoom = chatRooms.first else {
-                return
+            if let chatRoom = chatRooms.first {
+                onCompletion(chatRoom)
             }
-            onCompletion(chatRoom)
         }
     }
+
     static func getChatRooms(pairs: [FirebaseUserChatRoomModulePair],
                              user: User,
                              onCompletion: @escaping ([ChatRoom]) -> Void) {
-        let chatRoomIdStrs = pairs.map { $0.chatRoomId.val }
-        if chatRoomIdStrs.isEmpty {
+        let chatRoomIdStrings = pairs.map { $0.chatRoomId.val }
+
+        if chatRoomIdStrings.isEmpty {
             onCompletion([])
             return
         }
-        for chatRoomIdStrChunk in chatRoomIdStrs.chunked(into: 10) {
+
+        for chatRoomIdStringsChunk in chatRoomIdStrings.chunked(into: FirebaseUtils.queryChunkSize) {
             FirebaseUtils
                 .getEnvironmentReference(Firestore.firestore())
                 .collection(DatabaseConstant.Collection.chatRooms)
-                .whereField(DatabaseConstant.ChatRoom.id, in: chatRoomIdStrChunk)
+                .whereField(DatabaseConstant.ChatRoom.id, in: chatRoomIdStringsChunk)
                 .getDocuments { snapshots, error in
                     guard let documents = snapshots?.documents else {
-                        os_log("Getting chatrooms: ChatRooms with Id: \(chatRoomIdStrChunk) does not exist")
-                        os_log("Error \(error?.localizedDescription ?? "No error")")
+                        os_log("Getting chatrooms: ChatRooms with ID: \(chatRoomIdStringsChunk) does not exist")
+                        os_log("Error getting chatrooms (\(error?.localizedDescription ?? ""))")
                         return
                     }
+
                     let chatRooms: [ChatRoom] = documents.compactMap { document in
                         guard let chatRoomIdStr = document[DatabaseConstant.ChatRoom.id] as? String,
                               let pair = pairs.first(where: { $0.chatRoomId.val == chatRoomIdStr }) else {
                             os_log("ChatRoomQuery: Unable extract chatRoomId from \(document) or pair not in \(pairs)")
                             return nil
                         }
+
                         return FirebaseChatRoomAdapter.convert(
                             document: document,
                             user: user,
                             withPermissions: pair.permissions)
                     }
+
                     onCompletion(chatRooms)
                 }
         }
