0c5c26a6501c3b1948664711597f240803289c8c
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-04-18 22:36:37 +0800

    refactor: ChatRoomMediaCache as MediaMessageViewModelDelegate (#146)
    * refactor: chatroom media cache
    
    * refactor: remove 5 white spaces
    
    * chore: add example url

--- Sweechat/ViewModels/Message/MediaMessageViewModelDelegate.swift
+++ Sweechat/ViewModels/Message/MediaMessageViewModelDelegate.swift
@@ -3,6 +3,6 @@ import Foundation
 protocol MediaMessageViewModelDelegate: AnyObject {
     func fetchImageData(fromUrlString urlString: String,
                         onCompletion: @escaping (Data?) -> Void)
-    func fetchVideoLocalUrl(fromUrlString urlString: String,
-                            onCompletion: @escaping (URL?) -> Void)
+    func fetchLocalUrl(fromUrlString urlString: String,
+                       onCompletion: @escaping (URL?) -> Void)
 }
