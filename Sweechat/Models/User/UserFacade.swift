98a19b18a1adbb6b77fc07cdb7d7901f76e71ebe
Kevin Lim <lyskevin@gmail.com>
2021-04-18 11:08:11 +0800

    docs: update documentation for the UserFacade model

--- Sweechat/Models/User/UserFacade.swift
+++ Sweechat/Models/User/UserFacade.swift
@@ -1,5 +1,8 @@
 import FirebaseFirestore
 
+/**
+ An interface through which the `User` model comunicates with the server.
+ */
 protocol UserFacade {
     /// An abstraction through which the server communicates with the calling object instance.
     var delegate: UserFacadeDelegate? { get set }
