e6303aac28ef28495c7444b8d6068e530b855fcc
Agnes Natasya <agnesnatasyawijaya@gmail.com>
2021-04-18 16:00:54 +0800

    feat: change color of modal view (#138)
    * feat: change color of modal view
    
    * feat: profile picture display color
    
    * feat: profile pic url

--- Sweechat/Constants/DatabaseConstant.swift
+++ Sweechat/Constants/DatabaseConstant.swift
@@ -10,10 +10,10 @@ import Foundation
 struct DatabaseConstant {
     struct Collection {
         static let environmentCollection = "environment"
-        static let dev = "star-modules"
+        static let dev = "dev"
         // Change environmentDocument as needed when working on features
         // involving a schema change
-        static let environmentDocument = dev
+        static let environmentDocument = "dev"
 
         static let chatRooms = "chatRooms"
         static let messages = "messages"
