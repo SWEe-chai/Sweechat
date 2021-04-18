5bc3d0a1f9d6f4fb0bb2ab29d688385fe84ba760
Kevin Lim <lyskevin@gmail.com>
2021-04-18 15:12:07 +0800

    docs: add documentation for UserPermissionPair model

--- Sweechat/Models/ChatRoom/UserPermissionPair.swift
+++ Sweechat/Models/ChatRoom/UserPermissionPair.swift
@@ -1,3 +1,6 @@
+/**
+ Represents a `User` and his/her associated `ChatRoom` permissions.
+ */
 struct UserPermissionPair {
     let userId: Identifier<User>
     let permissions: ChatRoomPermissionBitmask
