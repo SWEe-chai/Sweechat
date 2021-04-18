8af8383d19357a719c739f872ef3a89cbeeabf91
Kevin Lim <lyskevin@gmail.com>
2021-04-18 21:38:40 +0800

    test: add message unit tests

--- SweechatTests/UnitTests/Models/Message/StringToDataTests.swift
+++ SweechatTests/UnitTests/Models/Message/StringToDataTests.swift
@@ -1,32 +1,20 @@
-//
-//  StringToDataTests.swift
-//  SweechatTests
-//
-//  Created by Kevin Lim on 18/4/21.
-//
-
 import XCTest
+@testable import Sweechat
 
 class StringToDataTests: XCTestCase {
+    private var sut: String!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        sut = "Test"
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testToData_returnsDataRepresentation() {
+        XCTAssertEqual(sut.toData(), Data(sut.utf8))
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
