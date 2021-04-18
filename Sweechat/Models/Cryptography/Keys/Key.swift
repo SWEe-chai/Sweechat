bcd59db5c9b4f59061f5f51817ea7261cdcd18a8
Kevin Lim <lyskevin@gmail.com>
2021-04-18 17:17:33 +0800

    docs: add documentation for cryptographic keys

--- Sweechat/Models/Cryptography/Keys/Key.swift
+++ Sweechat/Models/Cryptography/Keys/Key.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/**
+ An interface representing a cryptographic key.
+ */
 protocol Key {
     var rawRepresentation: Data { get }
 }
--- Sweechat/Models/Cryptography/Keys/P256PublicKey.swift
+++ Sweechat/Models/Cryptography/Keys/P256PublicKey.swift
@@ -1,13 +1,23 @@
 import CryptoKit
 import Foundation
 
+/**
+ A cryptographic public key generated using NIST P-256 elliptic curves.
+ */
 struct P256PublicKey: PublicKey {
     let rawRepresentation: Data
 
+    /// Constructs a `P256PublicKey` from the specified raw representation.
     init(rawRepresentation: Data) {
         self.rawRepresentation = rawRepresentation
     }
 
+    /// Returns true if the specified signature is valid for the specified data.
+    /// - Parameters:
+    ///   - signature: The specified signature.
+    ///   - data: The specified data.
+    /// - Returns: true if the specified signature is valid for the specified data.
+    /// - Throws: A `SignalProtocolError` if there is an error during signature verification.
     func isValidSignature(_ signature: Data, for data: Data) throws -> Bool {
         do {
             let signingKey = try P256.Signing.PublicKey(rawRepresentation: rawRepresentation)
