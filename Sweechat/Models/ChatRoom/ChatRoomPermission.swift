49a0a720069d89d0837ef868f16fc6299555c253
Kevin Lim <lyskevin@gmail.com>
2021-04-18 12:05:58 +0800

    docs: add documentation for ChatRoomPermission

--- Sweechat/Models/ChatRoom/ChatRoomPermission.swift
+++ Sweechat/Models/ChatRoom/ChatRoomPermission.swift
@@ -1,14 +1,10 @@
-//
-//  ChatRoomPermission.swift
-//  Sweechat
-//
-//  Created by Christian James Welly on 14/3/21.
-//
-
 import Foundation
 
 typealias ChatRoomPermissionBitmask = UInt32
 
+/**
+ Represents chat room permissions in the application.
+ */
 struct ChatRoomPermission {
     static let none: ChatRoomPermissionBitmask = 0
     static let read: ChatRoomPermissionBitmask = 0b1
@@ -22,10 +18,16 @@ struct ChatRoomPermission {
         ChatRoomPermission.readWrite |
         ChatRoomPermission.invite | ChatRoomPermission.pin
 
+    /// Returns true if the specified `ChatRoomPermissionBitmask` allows reading.
+    /// - Parameters:
+    ///  - permission: The specified `ChatRoomPermissionBitmask`.
     static func canRead(permission: ChatRoomPermissionBitmask) -> Bool {
         permission & read != 0
     }
 
+    /// Returns true if the specified `ChatRoomPermissionBitmask` allows writing.
+    /// - Parameters:
+    ///  - permission: The specified `ChatRoomPermissionBitmask`.
     static func canWrite(permission: ChatRoomPermissionBitmask) -> Bool {
         permission & write != 0
     }
