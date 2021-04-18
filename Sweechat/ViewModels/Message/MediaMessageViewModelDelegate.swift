e7a3119ed3b53d7ae492c7cb586be0851d09e16b
Hai Nguyen <50471593+helloImHai@users.noreply.github.com>
2021-04-18 22:30:41 +0800

    refactor: remove 5 white spaces

--- Sweechat/ViewModels/Message/MediaMessageViewModelDelegate.swift
+++ Sweechat/ViewModels/Message/MediaMessageViewModelDelegate.swift
@@ -4,5 +4,5 @@ protocol MediaMessageViewModelDelegate: AnyObject {
     func fetchImageData(fromUrlString urlString: String,
                         onCompletion: @escaping (Data?) -> Void)
     func fetchLocalUrl(fromUrlString urlString: String,
-                            onCompletion: @escaping (URL?) -> Void)
+                       onCompletion: @escaping (URL?) -> Void)
 }
