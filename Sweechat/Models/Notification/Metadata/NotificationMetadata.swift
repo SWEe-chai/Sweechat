91a94503816537c7d145e1b8cb66eb64ca6f0534
Kevin Lim <lyskevin@gmail.com>
2021-04-18 17:45:52 +0800

    docs: add documentation for NotificationMetaData

--- Sweechat/Models/Notification/Metadata/NotificationMetadata.swift
+++ Sweechat/Models/Notification/Metadata/NotificationMetadata.swift
@@ -1,36 +1,42 @@
-//
-//  NotificationMetadata.swift
-//  Sweechat
-//
-//  Created by Agnes Natasya on 14/4/21.
-//
-
 import Combine
 
+/**
+ Repesents metadata for push notifications.
+ */
 class NotificationMetadata {
-    @Published var isFromNotif: Bool
     var directChatRoomId: String
     var directModuleId: String
+
+    @Published var isFromNotif: Bool
+
     private var defaultIsFromNotifValue = false
     private var defaultDirectModuleIdValue = ""
     private var defaultDirectChatRoomIdValue = ""
 
+    /// Constructs an instance of `NotificationMetaData` with the specified information.
     init(isFromNotif: Bool, directModuleId: String, directChatRoomId: String) {
         self.isFromNotif = isFromNotif
         self.directModuleId = directModuleId
         self.directChatRoomId = directChatRoomId
     }
 
+    /// Constructs an instance of `NotificationMetaData`.
     init() {
         self.isFromNotif = defaultIsFromNotifValue
         self.directModuleId = defaultDirectModuleIdValue
         self.directChatRoomId = defaultDirectChatRoomIdValue
     }
 
+    /// Subscribes to the this `NotificationMetaData`'s `isFromNotif` flag
+    /// by executing the specified function on change to the flag.
+    /// - Parameters:
+    ///   - function: The specified function to execute on change to the profile picture.
+    /// - Returns: An `AnyCancellable` that executes the specified closure when cancelled.
     func subscribeToIsFromNotif(function: @escaping (Bool) -> Void) -> AnyCancellable {
         $isFromNotif.sink(receiveValue: function)
     }
 
+    /// Resets this `NotificationMetaData` to the default values.
     func reset() {
         self.isFromNotif = defaultIsFromNotifValue
         self.directModuleId = defaultDirectModuleIdValue
